0711ba7ae0b2b731bb87efdbc722ea9a55525855
Wyatt Sawyer <wyatt.sawyer@conduent.com>
2021-12-06 20:08:43 -0500

    highlight on nav bar links

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu"); 
+const navLogo = document.querySelector("#navbar__logo");
 
 // Display Mobile Menu
 const mobileMenu = () => {
@@ -8,3 +9,38 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const servicesMenu = document.querySelector('#services-page');
+    let scrollPos = window.scrollY;
+
+    // adds 'highlight' class to menu items
+    // If not on mobile (width < 960)
+
+    // Home is 0 -> 600 px... etc.
+    if(window.innerWidth > 960 && scrollPos < 600){ 
+        homeMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    } else if(window.innerWidth > 960 && scrollPos < 1400){ 
+        aboutMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        servicesMenu.classList.remove('highlight');
+        return;
+    } else if(window.innerWidth > 960 && scrollPos < 2345){ 
+        servicesMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    }
+
+    if((elem && window.innerWidth < 960 && scrollPos < 600) || elem){
+        elem.classList.remove('highlight');
+    }
+}
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
